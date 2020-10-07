599d592cc7aa1ac0f8aaf2fe5a6beea0cf39639a
Alvaro <a.pavonal@alumnos.urjc.es>
2020-10-07 21:25:44 +0200

    Movement script + Jump script concept + Player demo material

---
+++ wave/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxisInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(xAxisInput, 0, 0) * _speed * Time.deltaTime);
+
+        float zAxisInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, zAxisInput) * _speed * Time.deltaTime);
+    }
+}
